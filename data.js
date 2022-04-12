188101291f479479d8c3205ccc193fa2ae967d02
aline <aline.rosa.cruz@gmail.com>
2022-04-12 12:17:59 -0300

    Updates

--- data.js
+++ data.js
@@ -1,9 +1,19 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const ordemAlfabetica = (dadosPokemons, ordemEscolhida) => {
+  if (ordemEscolhida === "AZ") {
+    return dadosPokemons.sort((a, b) => (a.name > b.name ? 1 : -1))
+  } 
+    else {
+    return dadosPokemons.sort((a, b) => (a.name > b.name ? -1 : 1))
+  }
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const filtrarNome = (dadosPokemons, name) => {
+  return dadosPokemons.filter(pokemon => {
+    return pokemon.name.toLowerCase().includes(name.toLowerCase())
+  });
+}
+
+export const calcularPorcentagem = (total, raridadeEscolhida) => {
+  const porcentagem = Math.round((raridadeEscolhida * 100) / total).toFixed(2);
+  return porcentagem;
 };
--- main.js
+++ main.js
@@ -1,8 +1,50 @@
-import { example } from './data.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/lol/lol.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/ghibli/ghibli.js';
-// import data from './data/athletes/athletes.js';
+import { ordemAlfabetica, filtrarNome, calcularPorcentagem } from './data.js';
 
-console.log(example, data);
+import data from './data/pokemon/pokemon.js';
+
+let dadosPokemons = data.pokemon;
+
+function imprimePokemons(data) {
+    const card = document.getElementById("grupoCard");
+    card.innerHTML = data
+      .map(
+        (item) => `
+      <div class="card">
+        <img src="${item.img}" alt="${item.name}">
+        <div class="textCard">
+         <p class="titulo1 titulo1card">${item.name}</p> 
+         <p class="titulo3 titulo3card">Pokemon-rarity: <br> ${item["pokemon-rarity"]}</p>
+         <p class="titulo3 titulo3card">Distance egg: <br> ${item.egg}</p>
+        </div>
+      </div>`
+      )
+      .join("");
+  }
+  imprimePokemons(dadosPokemons);
+
+ const ordemAlfa = document.getElementById("filtroOrdenar"); 
+
+ ordemAlfa.addEventListener("change", (e) => {
+    const ordemEscolhida = e.target.value;
+    const filtroOrdem = ordemAlfabetica(dadosPokemons, ordemEscolhida);
+    imprimePokemons(filtroOrdem);
+  
+  })
+
+const voltarTopo = document.getElementById("voltarTopo");
+
+voltarTopo.addEventListener("click", (e) => {
+  window.scrollTo(0, 0);
+});
+
+const buscaNome = document.getElementById("filtroBuscar");
+
+buscaNome.addEventListener("keyup", (e) => {
+  const nomePreenchido = e.target.value;
+  const resultadoBusca = filtrarNome(dadosPokemons, nomePreenchido)
+  imprimePokemons(resultadoBusca)
+});
+
+const limpaFiltros = document.getElementById("limparFiltros")
+
+limpaFiltros.addEventListener("click", () => imprimePokemons(dadosPokemons));
