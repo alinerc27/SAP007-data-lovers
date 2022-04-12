6bf222f2e8f4ed07232af5aa9f51e203051119f6
aline <aline.rosa.cruz@gmail.com>
2022-04-12 08:55:21 -0300

    funções com comentarios, sem calculo

--- src/data.js
+++ src/data.js
@@ -1,9 +1,25 @@
-// estas funciones son de ejemplo
+//funcoes de filtro, ordenação, calculo
 
-export const example = () => {
-  return 'example';
+//colocar sub ordenação por ovo na quarta sprint
+export const ordemAlfabetica = (dadosPokemons, ordemEscolhida) => {
+  if (ordemEscolhida === "AZ") {
+    return dadosPokemons.sort((a, b) => (a.name > b.name ? 1 : -1))
+  } 
+    else {
+    return dadosPokemons.sort((a, b) => (a.name > b.name ? -1 : 1))
+  }
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//Apresenta cards de acordo com busca
+export const filtrarNome = (dadosPokemons, name) => {
+  return dadosPokemons.filter(pokemon => {
+    return pokemon.name.toLowerCase().includes(name.toLowerCase())
+  });
+}
+
+//Fazer calculo de porcentagem
+
+export const calcularPorcentagem = (total, raridadeEscolhida) => {
+  const porcentagem = Math.round((raridadeEscolhida * 100) / total).toFixed(2);
+  return porcentagem;
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,80 @@
-import { example } from './data.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/lol/lol.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/ghibli/ghibli.js';
-// import data from './data/athletes/athletes.js';
+//chamar funcoes de calculo filtro e etc
+import { ordemAlfabetica, filtrarNome, calcularPorcentagem } from './data.js';
 
-console.log(example, data);
+//aqui sera construidos os cards, dados vem desse arquivo
+import data from './data/pokemon/pokemon.js';
+
+//console.log(example, data);
+
+//na oh de sexta com o gabs perguntar se é let ou const pois ela se transforma nas funcs do data.js
+let dadosPokemons = data.pokemon;
+
+//trazendo todos os cards
+function imprimePokemons(data) {
+    const card = document.getElementById("grupoCard");
+    card.innerHTML = data
+      .map(
+        (item) => `
+      <div class="card">
+        <img src="${item.img}" alt="${item.name}">
+        <div class="textCard">
+         <p class="titulo1 titulo1card">${item.name}</p> 
+         <p class="titulo3 titulo3card">Pokemon-rarity: <br> ${item["pokemon-rarity"]}</p>
+         <p class="titulo3 titulo3card">Distance egg: <br> ${item.egg}</p>
+        </div>
+      </div>`
+      )
+      .join("");
+  }
+  imprimePokemons(dadosPokemons);
+
+//filtra/ordena por ordem alfabetica
+ const ordemAlfa = document.getElementById("filtroOrdenar"); 
+
+ ordemAlfa.addEventListener("change", (e) => {
+    const ordemEscolhida = e.target.value;
+    const filtroOrdem = ordemAlfabetica(dadosPokemons, ordemEscolhida);
+    imprimePokemons(filtroOrdem);
+  
+  })
+
+//volta p/ topo da pagina  
+const voltarTopo = document.getElementById("voltarTopo");
+
+voltarTopo.addEventListener("click", (e) => {
+  window.scrollTo(0, 0);
+});
+
+//busca por nome pesquisado
+
+const buscaNome = document.getElementById("filtroBuscar");
+
+buscaNome.addEventListener("keyup", (e) => {
+  const nomePreenchido = e.target.value;
+  const resultadoBusca = filtrarNome(dadosPokemons, nomePreenchido)
+  imprimePokemons(resultadoBusca)
+});
+
+/*
+//calculo por raridade
+
+const porcentagemRaridade = document.getElementById("filtroCalcular"); 
+
+porcentagemRaridade.addEventListener("change", (e) => {
+   const raridadeEscolhida = e.target.value;
+   const mostraPorcentagens = calcularPorcentagem(dadosPokemons, raridadeEscolhida);
+   imprimePokemons(mostraPorcentagens);
+ 
+ })
+
+const percentagem = document.getElementById("resultadoCalculo");
+
+function mostrarPorcentagem(percentagem) {
+    percentagem.innerHTML = `Essa é a porcentagem de Pokemons da raridade escolhida ${percentagem}`;
+}
+*/
+
+//final ---- limpar busca
+const limpaFiltros = document.getElementById("limparFiltros")
+
+limpaFiltros.addEventListener("click", () => imprimePokemons(dadosPokemons));
