61bb16bade970efa8f15ff271a2e6d30f9a9584d
aline <aline.rosa.cruz@gmail.com>
2022-04-12 09:01:59 -0300

    arquivos sem os comentarios

--- src/data.js
+++ src/data.js
@@ -1,6 +1,3 @@
-//funcoes de filtro, ordenação, calculo
-
-//colocar sub ordenação por ovo na quarta sprint
 export const ordemAlfabetica = (dadosPokemons, ordemEscolhida) => {
   if (ordemEscolhida === "AZ") {
     return dadosPokemons.sort((a, b) => (a.name > b.name ? 1 : -1))
@@ -10,14 +7,11 @@ export const ordemAlfabetica = (dadosPokemons, ordemEscolhida) => {
   }
 };
 
-//Apresenta cards de acordo com busca
 export const filtrarNome = (dadosPokemons, name) => {
   return dadosPokemons.filter(pokemon => {
     return pokemon.name.toLowerCase().includes(name.toLowerCase())
   });
 }
-
-//Fazer calculo de porcentagem
 
 export const calcularPorcentagem = (total, raridadeEscolhida) => {
   const porcentagem = Math.round((raridadeEscolhida * 100) / total).toFixed(2);
--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
-//chamar funcoes de calculo filtro e etc
 import { ordemAlfabetica, filtrarNome, calcularPorcentagem } from './data.js';
 
-//aqui sera construidos os cards, dados vem desse arquivo
 import data from './data/pokemon/pokemon.js';
 
-//console.log(example, data);
-
-//na oh de sexta com o gabs perguntar se é let ou const pois ela se transforma nas funcs do data.js
 let dadosPokemons = data.pokemon;
 
-//trazendo todos os cards
 function imprimePokemons(data) {
     const card = document.getElementById("grupoCard");
     card.innerHTML = data
@@ -28,7 +22,6 @@ function imprimePokemons(data) {
   }
   imprimePokemons(dadosPokemons);
 
-//filtra/ordena por ordem alfabetica
  const ordemAlfa = document.getElementById("filtroOrdenar"); 
 
  ordemAlfa.addEventListener("change", (e) => {
@@ -38,14 +31,11 @@ function imprimePokemons(data) {
   
   })
 
-//volta p/ topo da pagina  
 const voltarTopo = document.getElementById("voltarTopo");
 
 voltarTopo.addEventListener("click", (e) => {
   window.scrollTo(0, 0);
 });
-
-//busca por nome pesquisado
 
 const buscaNome = document.getElementById("filtroBuscar");
 
@@ -55,26 +45,6 @@ buscaNome.addEventListener("keyup", (e) => {
   imprimePokemons(resultadoBusca)
 });
 
-/*
-//calculo por raridade
-
-const porcentagemRaridade = document.getElementById("filtroCalcular"); 
-
-porcentagemRaridade.addEventListener("change", (e) => {
-   const raridadeEscolhida = e.target.value;
-   const mostraPorcentagens = calcularPorcentagem(dadosPokemons, raridadeEscolhida);
-   imprimePokemons(mostraPorcentagens);
- 
- })
-
-const percentagem = document.getElementById("resultadoCalculo");
-
-function mostrarPorcentagem(percentagem) {
-    percentagem.innerHTML = `Essa é a porcentagem de Pokemons da raridade escolhida ${percentagem}`;
-}
-*/
-
-//final ---- limpar busca
 const limpaFiltros = document.getElementById("limparFiltros")
 
 limpaFiltros.addEventListener("click", () => imprimePokemons(dadosPokemons));
